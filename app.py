e0646764799d21a93917b27c3eaa0757687b9a00
Panashe H Guzha <73646417+panashe-dot@users.noreply.github.com>
2024-02-24 14:16:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -64,8 +64,4 @@ if st.button("Predict Fraud"):
     "amount":amount
     }
     data_df = pd.DataFrame([data])
-    prediction = load_model_and_predict(data)[0]
-    if prediction >= 0.5:
-        st.success("**Potential Fraud Detected (Predicted: 1)**")
-    else:
-        st.info("**Low Fraud Risk (Predicted: 0)**")
+    st.write(data_df)
